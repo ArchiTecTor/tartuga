b854dfd479064fb87544bcd5bdddc9c04c5286cc
Diakonov Paul <xarchitectorx@gmail.com>
2012-07-13 06:36:03 +0400

    некоторые переработки

---
+++ lib/Tartuga/Errors/QueueFull.pm
@@ -0,0 +1,18 @@
+=head1 Tartuga::Errors::QueueFull
+
+ошибки при инициализации
+
+=cut
+
+package Tartuga::Errors::QueueFull;
+
+use strict;
+use warnings;
+use Tartuga::Exception;
+
+use base "Tartuga::Exception";
+
+our $VERSION = 0.001;
+
+
+1;
